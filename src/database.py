c4bc7193d69fffda691ee92c0334e1a02519a319
wotiwan <iwanpomogaev@yandex.ru>
2024-06-05 22:38:14 +0800

    database.py updated

--- src/database.py
+++ src/database.py
@@ -9,3 +9,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
