d5b8bc29eb9f5b5aa39bf03274c04021fd0fe482
shokorev25 <63199119+shokorev25@users.noreply.github.com>
2024-06-10 04:10:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mobiledogs',
-    version='0.1',
-    description='MobileDogs_API is a dog monitoring and management system',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    author='Ivan Pomogaev',
-    author_email='shokorev2003@gmail.com',
-    maintainer='Shokorev Aleksandr',
-    maintainer_email='shokorev2003@gmail.com',
-    url='https://github.com/wotiwan/mobiledogs',
-    packages=find_packages(),
-    install_requires=[
+   name='mobiledogs',
+   version='1.0',
+   description='MobileDogs_API is a dog monitoring and management system.',
+   license='MIT',
+   author='shokorev25',
+   author_email='shokorev25@gmail.com',
+   url='https://github.com/wotiwan/mobiledogs',
+   packages=find_packages(),
+   install_requires=[
         'fastapi~=0.111.0',
         'uvicorn',
         'sqlalchemy~=2.0.30',
         'pydantic~=2.7.3',
         'mysql-connector-python',
-        'requests',
+        'requests'
         'sphinx',
         'sphinx-autobuild',
         'sphinx_rtd_theme',
     ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
+   extras_require={
+        'test': [
+            'pytest',
+            'coverage',
+        ],
+   },
+   python_requires='>=3',
 )
